06c799c03460b6e4f6ed7dc0f617536bc7a4b472
mghebru <mghebru123@gmail.com>
2026-02-04 21:11:02 -0500

    Delete src/src/utils directory

--- src/src/utils/taxCalculator.ts
+++
@@ -1,8 +0,0 @@
-import Product from "../models/Product.ts";
-export function calculateTax(product: Product): number{
-    // product: Object;
-    
-   return product.price *  (1.10);
-}
-  // export const calculateTax = (product: Product): 
-  // number => { return product.price * 1.10; };
