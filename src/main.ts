42d6ed7b330214ff01f99adae1235b9004cb5776
mghebru <mghebru123@gmail.com>
2026-02-04 20:53:56 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import {PhysicalProduct} from "./models/PhysicalProduct.ts";
 import {DigitalProduct} from "./models/DigitalProduct.ts";
+import { sortProducts } from "./models/productSorter.ts";
 
+//physical product
+const physical = new PhysicalProduct('56432', 'cereal', 40, 2.5);
+console.log(physical.displayDetails());
+console.log("Price with tax:", physical.getPriceWithTax(0.1));
+console.log("Weight:", physical.formattedWeight);
 
-const abc = new PhysicalProduct('56432', 'cereal', 40, 2.5);
-console.log(abc.displayDetails());
+//apply bulk discount if applicable
+physical.applyBulkDiscount();
 
-const def1 = new DigitalProduct('56432', 'cereal', 40, 200);
-console.log(def1.displayDetails());
+//digital product
+const digital = new DigitalProduct('56432', 'cereal', 40, 200);
+console.log(digital.displayDetails());
+console.log("Price with tax:", digital.getPriceWithTax(0.05));
+
+//store products together
+const inventory = [physical, digital];
+
+//sort products
+const sortedByPrice = sortProducts(inventory, "price");
+const sortedByName = sortProducts(inventory, "name");
+
+console.log("Sorted by price:", sortedByPrice);
+console.log("Sorted by name:", sortedByName);
